f5010707a09335426dcd54b8e4e806e94196bb04
thierry <thdi.4449@gmail.com>
2021-08-22 09:07:30 +0200

    Cleanup

--- src/include/app/Application.hpp
+++ src/include/app/Application.hpp
@@ -3,7 +3,6 @@
 
 #include "scene/Scene.hpp"
 #include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/System/Clock.hpp>
 #include <SFML/System/Time.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/ContextSettings.hpp>
@@ -15,130 +14,107 @@
 
 class Application
 {
-    public:
-	Application(float framePerSecond = 60.f)
-	  : FPS_LIMIT(1.f / framePerSecond)
-	  , mLastUpdateTime(0.f)
-	  , mLastFrameTime(0.f)
-	  , mWindowPtr(new sf::RenderWindow)
-	  , windowOpened(true)
-	  , currentScene(nullptr)
-	{
-	}
+  public:
+    Application(float framePerSecond = 60.f)
+      : FPS_LIMIT(framePerSecond)
+      , mWindowPtr(new sf::RenderWindow)
+      , windowOpened(true)
+      , currentScene(nullptr)
+    {
+    }
 
-	Application(sf::ContextSettings ctx, float framePerSecond = 60.f)
-	  : FPS_LIMIT(1.f / (framePerSecond))
-	  , mLastUpdateTime(0.f)
-	  , mLastFrameTime(0.f)
-	  , mWindowPtr(new sf::RenderWindow)
-	  , windowOpened(true)
-	  , currentScene(nullptr)
-	{
-	}
+    Application(sf::ContextSettings ctx, float framePerSecond = 60.f)
+      : FPS_LIMIT(framePerSecond)
+      , mWindowPtr(new sf::RenderWindow)
+      , windowOpened(true)
+      , currentScene(nullptr)
+    {
+    }
 
-	virtual ~Application()
-	{}
+    virtual ~Application()
+    {}
 
-	virtual void setup()
-	{}
+    virtual void setup()
+    {}
 
-	void setPosition(const int x, const int y)
-	{
-	  window()->setPosition(sf::Vector2i(x, y));
-	}
+    void setPosition(const int x, const int y)
+    {
+      window()->setPosition(sf::Vector2i(x, y));
+    }
 
-	void run(const sf::VideoMode& videoMode = sf::VideoMode(1280, 720)
-		 , const std::string& title = ""
-		 , const unsigned int style = sf::Style::Close | sf::Style::Resize
-		 , sf::ContextSettings* ctx = nullptr)
-	{
-	    init(videoMode, title, style, ctx);
-	    setup();
+    void run(const sf::VideoMode& videoMode = sf::VideoMode(1280, 720)
+	     , const std::string& title = ""
+	     , const unsigned int style = sf::Style::Close | sf::Style::Resize
+	     , sf::ContextSettings* ctx = nullptr)
+    {
+      init(videoMode, title, style, ctx);
+      setup();
+      window()->setFramerateLimit(static_cast<unsigned int>(FPS_LIMIT));
+      const float dt = 1.f / FPS_LIMIT;
 
-	    bool draw = false;
+      while(mWindowPtr.get()->isOpen()) {
+	sf::Event event;
 
-	    while(mWindowPtr.get()->isOpen()) {
-		sf::Event event;
+	currentScene->preHandleEvent();
 
-		currentScene->preHandleEvent();
-
-		while(mWindowPtr.get()->pollEvent(event)) {
-		  if (event.type == sf::Event::Closed)
-		  {
-		    windowOpened = false;
-		    mWindowPtr.get()->close();
-		    break;
-		  }
-		  if(windowOpened) {
-		    currentScene->handleEvent(event);
-		  }
-		}
-
-		if(!windowOpened) {
-		  break;
-		}
-
-		currentScene->postHandleEvent();
-		currentScene->preUpdate(FPS_LIMIT);
-
-		float now = mClock.getElapsedTime().asSeconds();
-
-		if(now - mLastFrameTime >= FPS_LIMIT) {
-		  draw = true;
-		  mLastFrameTime = now;
-		  // update here
-		  if(!mWindowPtr.get()->isOpen()) {
-		      break;
-		  }
-
-		  currentScene->update(FPS_LIMIT);
-		}
-
-		mLastUpdateTime = now;
-		if(draw)
-		{
-		  draw = false;
-		  mWindowPtr.get()->clear();
-		  currentScene->draw();
-
-		  mWindowPtr.get()->display();
-		}
-
-	    }
+	while(mWindowPtr.get()->pollEvent(event)) {
+	  if (event.type == sf::Event::Closed)
+	  {
+	    windowOpened = false;
 	    mWindowPtr.get()->close();
+	    break;
+	  }
+	  if(windowOpened) {
+	    currentScene->handleEvent(event);
+	  }
 	}
 
-	sf::RenderWindow* window()
-	{
-	    return mWindowPtr.get();
+	if(!windowOpened) {
+	  break;
 	}
 
-    protected:
-	const float FPS_LIMIT;
-	float mLastUpdateTime;
-	float mLastFrameTime;
-	const sf::Clock mClock;
-	std::unique_ptr<sf::RenderWindow> mWindowPtr;
-	bool windowOpened;
-	Scene* currentScene;
+	currentScene->postHandleEvent();
+	currentScene->preUpdate(dt);
 
-    private:
-	void init(const sf::VideoMode& videoMode
-		  , const std::string& title
-		  , const unsigned int style
-		  , sf::ContextSettings* ctxPtr)
-	{
-	    sf::ContextSettings ctx;
-	    if(ctxPtr != nullptr) {
-		ctx = *ctxPtr;
-	    }
-
-	    window()->create(videoMode, title, style, ctx);
-/*
-	    window()->setPosition(sf::Vector2i(sf::VideoMode::getDesktopMode().width / 2 - window()->getSize().x / 2,
-					       sf::VideoMode::getDesktopMode().height / 2 - window()->getSize().y / 2));
-*/
+	// update here
+	if(!mWindowPtr.get()->isOpen()) {
+	    break;
 	}
+
+	currentScene->update(dt);
+
+	mWindowPtr.get()->clear();
+	currentScene->draw();
+
+	mWindowPtr.get()->display();
+      }
+      mWindowPtr.get()->close();
+    }
+
+    sf::RenderWindow* window()
+    {
+      return mWindowPtr.get();
+    }
+
+  protected:
+    const float FPS_LIMIT;
+    std::unique_ptr<sf::RenderWindow> mWindowPtr;
+    bool windowOpened;
+    Scene* currentScene;
+
+  private:
+    void init(const sf::VideoMode& videoMode
+	      , const std::string& title
+	      , const unsigned int style
+	      , sf::ContextSettings* ctxPtr)
+    {
+      sf::ContextSettings ctx;
+      if(ctxPtr != nullptr) {
+	  ctx = *ctxPtr;
+      }
+
+      window()->create(videoMode, title, style, ctx);
+    }
 };
 
 #endif // APPLICATION_HPP
--- src/include/scene/Scene.hpp
+++ src/include/scene/Scene.hpp
@@ -10,98 +10,98 @@
 
 class Scene
 {
-    public:
-	Scene()
-	  : mTitle("")
-	  , mousePos({0.f, 0.f})
-	  , window(nullptr)
-	  , enableMouseEvent(true)
-	  , mRunning(false)
-	{}
+  public:
+    Scene()
+      : mTitle("")
+      , mousePos({0.f, 0.f})
+      , window(nullptr)
+      , enableMouseEvent(true)
+      , mRunning(false)
+    {}
 
-	Scene(sf::RenderWindow* window)
-	  : mTitle("")
-	  , mousePos({0.f, 0.f})
-	  , window(window)
-	  , enableMouseEvent(true)
-	  , mRunning(false)
-	{
-	}
+    Scene(sf::RenderWindow* window)
+      : mTitle("")
+      , mousePos({0.f, 0.f})
+      , window(window)
+      , enableMouseEvent(true)
+      , mRunning(false)
+    {
+    }
 
-	virtual ~Scene()
-	{
-	}
+    virtual ~Scene()
+    {
+    }
 
-	virtual void preUpdate(const float dt)
-	{}
-	virtual void update(const float dt) = 0;
-	virtual void draw() = 0;
-	virtual void preHandleEvent()
-	{}
-	virtual void handleEvent(sf::Event& event) = 0;
-	virtual void postHandleEvent()
-	{}
-	virtual void load()
-	{
-	    mRunning = true;
-	}
+    virtual void preUpdate(const float dt)
+    {}
+    virtual void update(const float dt) = 0;
+    virtual void draw() = 0;
+    virtual void preHandleEvent()
+    {}
+    virtual void handleEvent(sf::Event& event) = 0;
+    virtual void postHandleEvent()
+    {}
+    virtual void load()
+    {
+	mRunning = true;
+    }
 
-	const bool isRunning()
-	{
-	    return mRunning;
-	}
+    const bool isRunning()
+    {
+      return mRunning;
+    }
 
-	void setWindow(sf::RenderWindow* window)
-	{
-	    this->window = window;
-	}
+    void setWindow(sf::RenderWindow* window)
+    {
+      this->window = window;
+    }
 
-	const std::string& getTitle() const
-	{
-	    return mTitle;
-	}
+    const std::string& getTitle() const
+    {
+      return mTitle;
+    }
 
-	void setTitle(const std::string& title)
-	{
-	    mTitle = title;
-	}
+    void setTitle(const std::string& title)
+    {
+      mTitle = title;
+    }
 
-    protected:
-	std::string mTitle;
-	sf::Vector2f mousePos;
-	sf::RenderWindow* window;
-	bool enableMouseEvent;
-	bool mRunning;
+  protected:
+    std::string mTitle;
+    sf::Vector2f mousePos;
+    sf::RenderWindow* window;
+    bool enableMouseEvent;
+    bool mRunning;
 
-	const sf::Vector2f getWindowCenter() const
-	{
-	    return static_cast<sf::Vector2f>(window->getSize()) * 0.5f;
-	}
+    const sf::Vector2f getWindowCenter() const
+    {
+      return static_cast<sf::Vector2f>(window->getSize()) * 0.5f;
+    }
 
-	const sf::Vector2f getWindowSize() const
-	{
-	    return static_cast<sf::Vector2f>(window->getSize());
-	}
+    const sf::Vector2f getWindowSize() const
+    {
+      return static_cast<sf::Vector2f>(window->getSize());
+    }
 
-	const float getWindowWidth() const
-	{
-	    return static_cast<float>(window->getSize().x);
-	}
+    const float getWindowWidth() const
+    {
+      return static_cast<float>(window->getSize().x);
+    }
 
-	const float getWindowHeight() const
-	{
-	    return static_cast<float>(window->getSize().y);
-	}
+    const float getWindowHeight() const
+    {
+      return static_cast<float>(window->getSize().y);
+    }
 
-	const float getWindowRatio() const
-	{
-	    return getWindowWidth() / getWindowHeight();
-	}
+    const float getWindowRatio() const
+    {
+      return getWindowWidth() / getWindowHeight();
+    }
 
-	const sf::Vector2f getMousePosition() const
-	{
-	    return static_cast<sf::Vector2f>(sf::Mouse::getPosition(*window));
-	}
+    const sf::Vector2f getMousePosition() const
+    {
+      return static_cast<sf::Vector2f>(sf::Mouse::getPosition(*window));
+    }
 };
 
 #endif // SCENE_HPP
